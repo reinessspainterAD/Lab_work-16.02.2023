45adaf4aa86cbf937eac70dc531c8b07f998faa3
Daniel Abdullin <reinesssspainter03@gmail.com>
2023-02-16 17:25:41 +0300

    Lab_work 16.02.2023

--- 3.js
+++ 3.js
@@ -1,15 +1,15 @@
 "use strict";
 const roadMines = [false, true, false, false, false, false, true, false, true, false];
-let hpTanck = 2;
+let hpTank = 2;
 for (let index = 0; index < roadMines.length; index++) {
-  if (hpTanck === 0) {
+  if (hpTank === 0) {
     break;
   } else if (roadMines[index]) {
-    hpTanck = hpTanck - 1;
+    hpTank = hpTank - 1;
   }
   if (!roadMines[index]) {
     console.log(`Танк переместился на ${index + 1}`);
-  } else if (hpTanck === 1 && roadMines[index]) {
+  } else if (hpTank === 1 && roadMines[index]) {
     console.log("Танк повреждён");
   } else {
     console.log("Танк уничтожен");
